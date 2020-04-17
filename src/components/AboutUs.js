695589e0129228f74e9ac46514283eeab8eb25cd
tomekdra <tomekdrabek@interia.pl>
2020-04-17 16:58:49 +0200

    Finished section AboutUs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,9 +6,10 @@ const AboutUs = () => {
     return (
         <div className={"wrapper"}>
             <div className={"aboutus"}>
-                <div className={"half-page"}>
+                <div className={"aboutus-content half-page"}>
                     <h2>O nas</h2>
                     <div className={"decoration"}></div>
+                    <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
                 </div>
                 <div className={"people-img"}>
 
--- src/components/FourSteps.js
+++ src/components/FourSteps.js
@@ -7,7 +7,7 @@ const FourSteps = () => {
     return (
         <div className={"container"}>
             <div className={"four-steps"}>
-                <h2>Wystarcza 4 proste kroki</h2>
+                <h2>Wystarczą 4 proste kroki</h2>
                 <div className={"decoration"}></div>
                 <div className={"row"}>
                     <div className={"col"}>
